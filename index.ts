857528bf2a282c714d506e247a5cd9bcfbb4a7e4
Chris Krycho <hello@chriskrycho.com>
2019-01-31 11:21:08 -0800

    Build times are NOT REASONABLE

--- index.ts
+++ index.ts
@@ -1,61 +1,6 @@
-// -- Make a minimal`Maybe` type just so we have an example of something generic
-// that we can use below
-class Just<T> {
-  constructor(private value: T) {}
-
-  map<U>(fn: (t: T) => U): Maybe<U> {
-    return new Just(fn(this.value));
-  }
-}
-
-class Nothing<T> {
-  map<U>(fn: (t: T) => U): Maybe<U> {
-    return (this as unknown) as Nothing<U>;
-  }
-}
-
-type Maybe<T> = Just<T> | Nothing<T>;
-
-// -- These types are just here because they give me a foundation to use with
-// building up the complex type below which causes the explosion in build time.
-type NonNullableFieldNames<T> = {
-  [K in keyof T]: T[K] extends NonNullable<T[K]> ? K : never
-}[keyof T];
-
-type NullableFieldNames<T> = Exclude<keyof T, NonNullableFieldNames<T>>;
-
-type NonNullableFields<T> = Pick<T, NonNullableFieldNames<T>>;
-
-type NullableFields<T> = Pick<T, Exclude<keyof T, NonNullableFieldNames<T>>>;
-
-// -- These are from a form validation library where I saw this blow up. I have
-// removed the vast majority of the complexity there.
-class OptionalField<T> {
-  readonly value?: T;
-  readonly isRequired: false = false;
-}
-
-class RequiredField<T> {
-  readonly value?: T;
-  readonly isRequired: true = true;
-}
-
-// -- Now, some conditional types interacting with this generic type, which will
-// will cause *serious* compile time degradation ever since conditional types
-// were introduced in 2.8, but especially after 3.1.
-type Form<T> = Required<
-  {
-    [K in keyof T]: K extends NonNullableFieldNames<T>
-      ? T[K] extends Maybe<infer U>
-        ? OptionalField<U>
-        : RequiredField<T[K]>
-      : OptionalField<NonNullable<T[K]>>
-  }
->;
-
-type FromModel<T> = (
-  model: T extends Maybe<infer U> ? Maybe<Partial<U>> : Partial<T>
-) => Form<T extends Maybe<infer U> ? U : T>;
+import Maybe from "true-myth/maybe";
+import Form, { FromModel } from "@olo/principled-forms/form";
+import Field from "@olo/principled-forms/field";
 
 // -- That sufficiently sets up the example to demonstrate how performance
 // degrades. To demonstrate the performance regression, simply
@@ -63,10 +8,22 @@ interface User {
   age: number;
   name?: string;
   email: Maybe<string>;
+  spouse?: User;
 }
 
 const formFromUser: FromModel<User> = user => ({
-  age: new RequiredField(),
-  name: new OptionalField(),
-  email: new OptionalField()
+  age: Field.required({ value: user.age }),
+  name: Field.optional({ value: user.name }),
+  email: Field.optional({
+    value: user.email
+      ? user.email.match({ Just: v => v, Nothing: () => undefined })
+      : undefined
+  }),
+  spouse: Field.optional({ value: user.spouse })
+});
+
+let a: Form<User> = formFromUser({
+  age: 31,
+  name: "Chris",
+  email: Maybe.just("hello@chriskrycho.com")
 });
